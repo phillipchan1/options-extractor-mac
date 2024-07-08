64ae449f5bdf93cbf6d03f99df25a30d653f59ba
Phil Chan <phillipchan1@gmail.com>
2024-07-08 11:27:56 -0500

    Initial Commit

---
+++ OptionsExtractor/OptionsExtractorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptionsExtractorApp.swift
+//  OptionsExtractor
+//
+//  Created by Phil Chan on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct OptionsExtractorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
